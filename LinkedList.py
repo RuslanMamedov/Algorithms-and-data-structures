da93607941f11d0f8be4face948a6885d5308c58
RuslanMamedov <41539922+RuslanMamedov@users.noreply.github.com>
2018-10-23 20:32:00 -0700

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -160,3 +160,51 @@ def Kth(head, k): #O(2n)
         p=p.next
     return head
 print('5',Kth(node1,2))
+node1 = Node('1') 
+node2 = Node('2') 
+node3 = Node('3')
+node4 = Node('4') 
+node5 = Node('5') 
+node6 = Node('6') 
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+#6. Reverse a linked list reccursively
+#iterative approach - 3 pointers 
+def ReverseIter(head):
+    if head ==None:
+        return False
+    if head.next==None:
+        return head
+    
+    p1=head
+    p2=head.next
+    head.next=None
+    p3=p2.next
+    while p3!=None:
+        p2.next=p1
+        p1=p2
+        p2=p3
+        p3=p3.next
+    p2.next=p1
+    p3=p2
+    while p2!= None:
+        print (p2.data)
+        p2=p2.next
+    return p3
+print ('#6.',ReverseIter(node1))
+#recursive approach - starting from the tail
+def ReverseRec (head):
+    if head==None or head.next == None:
+        return head
+    prev = ReverseRec(head.next)
+    head.next.next = head
+    head.next = None
+    return prev
+print ('#6.')
+p=ReverseRec(node6) #reversing the reversed linked list
+while p!=None:
+    print (p.data)
+    p=p.next
